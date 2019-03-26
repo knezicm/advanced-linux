7fd7b6462aea3bd549c808f6e22e26451ba6a39e
Mladen Knezic <mladen.knezic@gmail.com>
2019-03-26 13:18:25 +0100

    Fixed the issue with baudrate settings in serial example

--- module-08/serial.c
+++ module-08/serial.c
@@ -25,26 +25,16 @@ int main ()
 		printf("Error while getting configuration.\n");
 		return -1;
 	}
-
-	config.c_iflag = 0;
-	config.c_oflag = 0;
-	config.c_lflag = 0;
-	config.c_cflag = CS8 | CREAD | CLOCAL;
-	config.c_cc[VMIN] = 1;
-	config.c_cc[VTIME] = 0;
+	
+	// make raw config
+	cfmakeraw(&config);
 
 	// set the speed
-	if (cfsetispeed(&config, B9600) < 0)
-	{
-		printf("Error while trying to set in-line speed.\n");
-		return -1;
-	}
+	cfsetispeed(&config, B9600);
+	cfsetospeed(&config, B9600);
 
-	if (cfsetospeed(&config, B9600 < 0))
-	{
-		printf("Error while trying to set out-line speed.\n");
-		return -1;
-	}
+	config.c_cc[VMIN] = 1;
+	config.c_cc[VTIME] = 0;
 
 	// Apply the config
 	if (tcsetattr(fd, TCSANOW, &config) < 0)
