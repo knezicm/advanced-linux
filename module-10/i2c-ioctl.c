d3cc2a892f1d28044c1da54d93bcf286d8b28056
Mladen Knezic <mladen.knezic@gmail.com>
2019-03-29 13:18:09 +0100

    Added ioctl example for i2c. Updated clasic read/write i2c example.

--- module-10/i2c-ioctl.c
+++ module-10/i2c-ioctl.c
@@ -10,6 +10,26 @@ int main()
 	unsigned char reg = 0x10;
 	unsigned char rx_buffer[10];
 	unsigned char tx_buffer[10];
+	
+	struct i2c_msg iomsgs[] = {
+		[0] = {
+			.addr = addr,		/* slave address */
+			.flags = 0,		/* write access */
+			.buf = &reg,
+			.len = 1
+		},
+		[1] = {
+			.addr = addr,		/* slave address */
+			.flags = I2C_M_RD,	/* read access */
+			.buf = rx_buffer,
+			.len = 1		
+		},
+	};
+	
+	struct i2c_rdwr_ioctl_data msgset = {
+		.msgs = iomsgs,
+		.nmsgs = 2
+	};
 
 	// try to open i2c device
 	fd = open("/dev/i2c-0", O_RDWR);
@@ -21,25 +41,11 @@ int main()
 		return -1;
 	}
 
-	// set the address of i2c device
-	if (ioctl(fd, I2C_SLAVE, addr) < 0)
-	{
-		printf("Error while trying to set the address of i2c device.\n");
-		return -1;
-	}
-
 	// data exchange forever
 	for (;;)
 	{
-		// fill the tx buffer
-		tx_buffer[0] = reg;
-
-		// write the data (reg address)
-		if (write(fd, tx_buffer, 1) != 1)
-			printf("Failed to write data.\n");
-			
 		// read data from addressed register
-		if (read(fd, rx_buffer, 1) != 1)
+		if (ioctl(fd, I2C_RDWR, &msgset) < 0)
 			printf("Failed to read data.\n");
 		else
 			printf("Data read from reg 0x%x is 0x%x.\n", reg, rx_buffer[0]);
